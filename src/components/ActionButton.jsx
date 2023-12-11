f97942df77095dcdefac0f8e2223f4170a8a9f64
rezafikkri <fikkri.reza@gmail.com>
2023-12-11 12:24:02 +0700

    refactor: create ActionButton component for archive and delete action in DetailPage component

---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Icon from '@mdi/react';
+
+export default function ActionButton({ id, onClick, btnClass, iconPath, title }) {
+  return (
+    <button
+      type="button"
+      className={`btn btn-sm ${btnClass}`}
+      title={title}
+      onClick={() => onClick(id)}
+    >
+      <Icon path={iconPath} size={0.85} />
+    </button>
+  );
+}
+
+ActionButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  btnClass: PropTypes.string.isRequired,
+  iconPath: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
--- src/components/DetailAction.jsx
+++ src/components/DetailAction.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Icon from '@mdi/react';
 import { mdiArchiveArrowDown, mdiArrowLeftThick, mdiDelete } from '@mdi/js';
+import ActionButton from "./ActionButton";
 
 export default function DetailNoteAction({ id, onDelete }) {
   return (
@@ -12,14 +13,13 @@ export default function DetailNoteAction({ id, onDelete }) {
       <button type="button" className="btn btn-sm btn-outline-secondary mb-5" title="Archive catatan">
         <Icon path={mdiArchiveArrowDown} size={0.85} />
       </button>
-      <button
-        type="button"
-        onClick={() => onDelete(id)}
-        className="btn btn-sm btn-outline-danger"
-        title="Hapus catatan"
-      >
-        <Icon path={mdiDelete} size={0.85} />
-      </button>
+      <ActionButton
+        id={id}
+        onClick={onDelete}
+        btnClass="btn-outline-danger"
+        iconPath={mdiDelete}
+        title="delete"
+      />
     </div>
   );
 }

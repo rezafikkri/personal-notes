4bab5099c62b0753d8a78db7eb3f5f90f36bbf61
rezafikkri <fikkri.reza@gmail.com>
2023-12-13 13:27:51 +0700

    refactor: use " rather than ' for string in code

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import ContentEditable from 'react-contenteditable'
+import ContentEditable from "react-contenteditable"
 import { Link } from "react-router-dom";
 
 export default class NoteInput extends React.Component {

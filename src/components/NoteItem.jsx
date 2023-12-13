dabd066b19325b4158daf4f6a6954e313e6aa651
rezafikkri <fikkri.reza@gmail.com>
2023-12-13 11:28:17 +0700

    refactor: remove link for note item title and make navigate to detail page when click on card

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,17 +1,22 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { showFormattedDate, stripHtml } from "../utils";
 
 export default function NoteItem({ id, title, body, createdAt }) {
   const stripedBody = stripHtml(body);
   const bodyCountChr = stripedBody.length;
   const dots = bodyCountChr <= 80 ? "" : "...";
+  const navigate = useNavigate();
+
+  function handleClickCard() {
+    navigate(`/notes/${id}`);
+  }
 
   return (
     <div className="col-lg-6">
-      <div className="card">
+      <div className="card" onClick={handleClickCard}>
         <div className="card-body">
-          <h2 className="fs-4"><Link to={`/notes/${id}`} className="text-body">{title}</Link></h2>
+          <h2 className="fs-4">{title}</h2>
           <time className="text-body-secondary" dateTime={createdAt}>{showFormattedDate(createdAt)}</time>
           <p className="mt-3 mb-0">{stripedBody.substring(0, 80) + dots}</p>
         </div>
